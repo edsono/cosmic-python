1ce65f4e635caeda621a1652b30f4c40df2940ce
Edson César <edsono@gmail.com>
2021-10-05 07:11:14 -0400

    Remove os testes de uso de Copyright do Intellij

--- allocation/__init__.py
+++ allocation/__init__.py
@@ -1,13 +1,3 @@
-#  Direitos Autorais (c) 2021 Edson César.
-#
-#  Proibida a cópia parcial ou total de qualquer dos arquivos de código fonte
-#  deste projeto. Proibido o uso comercial ou com finalidades lucrativas em
-#  qualquer hipótese. Esta licença está baseada em estudos sobre a Lei
-#  Brasileira de Direitos Autorais (Lei 9.610/1998) e Tratados Internacionais
-#  sobre Propriedade Intelectual.
-
-#
-#
 from .batches import allocate
 from .batches import Batch
 from .batches import OrderLine
